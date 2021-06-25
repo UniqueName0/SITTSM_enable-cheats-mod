2a1543f569507b11aa97090a3ad251ac14a8cb03
UniqueName <75515676+UniqueName0@users.noreply.github.com>
2021-06-24 22:28:57 -0400

    yes

---
+++ Class1.cs
@@ -0,0 +1,35 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+using System;
+using System.Reflection;
+using HarmonyLib.Tools;
+using BepInEx.Harmony;
+
+namespace enable_cheats
+{
+    [BepInProcess("Stick It To The (Stick) Man.exe")]
+    [BepInPlugin("uniquename.sittsm.enable-cheats", "enable-cheats", "0.0.0.0")]
+    public class cheats_enable : BaseUnityPlugin
+    {
+
+        public void Awake()
+        {
+            var harmony = new Harmony("uniquename.sittsm.enable-cheats");
+            harmony.PatchAll();
+        }
+
+        public const string modID = "uniquename.sittsm.enable-cheats";
+        public const string modName = "enable-cheats";
+    }
+
+    [HarmonyPatch(typeof(Application), "get_isEditor")]
+    public class IsEditor_patch
+    {
+        public static bool Prefix(ref bool __result)
+        {
+            __result = true;
+            return false;
+        }
+    }
+}
